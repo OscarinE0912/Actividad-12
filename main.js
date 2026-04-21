158ec7b1e71bfef21533847ff6c7c4c3dc1f13ef
Oscar0912 <Oscarineduardorivera@gmail.com>
2026-04-20 20:26:57 -0700

    Task10

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
+Vue.component('chuck-card', {
+  props: ['icon_url', 'value'],
+  template: `
+    <div class="card h-100 chuck-card">
+      <img :src="icon_url" class="card-img-top chuck-img" :alt="value">
+      <div class="card-body">
+        <p class="card-text">{{ value }}</p>
+      </div>
+    </div>
+  `
+})
+
 var app = new Vue({
   el: '#app',
   data() {
     return {
       chuck: [
-        { value: "Chuck Norris can skydive into outer space." },
-        { value: "The chief export of Chuck Norris is pain." },
-        { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
-        { value: "Time waits for no man. Unless that man is Chuck Norris." },
-        { value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
-      ]
+        {
+            icon_url: "chucknorris.jpeg",
+            value: "Chuck Norris can skydive into outer space."
+        },
+        {
+            icon_url: "chucknorris.jpeg",
+            value: "The chief export of Chuck Norris is pain."
+        },
+        {
+            icon_url: "chucknorris.jpeg",
+            value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants."
+        },
+        {
+            icon_url: "chucknorris.jpeg",
+            value: "Time waits for no man. Unless that man is Chuck Norris."
+        },
+        {
+            icon_url: "chucknorris.jpeg",
+            value: "If you spell Chuck Norris in Scrabble, you win. Forever."
+        }
+        ]
     }
   }
 })
